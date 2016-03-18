d14b10605a03914ce04984bbb2be75682d68ec31
Olegnd <olegnd63@gmail.com>
2016-03-18 18:20:59 +0200

    modification 1 base.element.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-﻿'use strict';
+'use strict';
 
 var BaseElement = require('./js/base.element.js');
-var Food = require('./js/food.js');
-var SnakePart = require('./js/snake.part.js');
-var Snake = require('./js/snake.js');
+//var Food = require('./js/food.js');
+//var SnakePart = require('./js/snake.part.js');
+//var Snake = require('./js/snake.js');
 
-console.log(new BaseElement());
-console.log(new Food());
-console.log(new SnakePart());
-console.log(new Snake());
+var elements = {
+    x : 10,
+    y : 10,
+    isVisible : false
+};
+
+console.log(new BaseElement(elements));
+//console.log(new Food());
+//console.log(new SnakePart());
+//console.log(new Snake());
--- src/js/base.element.js
+++ src/js/base.element.js
@@ -1,3 +1,14 @@
-﻿'use strict';
+'use strict';
 
+//code Olegnd
+
+
+module.exports = function BaseElement (element) {
+    element.x = (element.x === undefined) ? this.x : 0
+    this.x = 0 || this.x = element.x
+    element.y = (element.x === undefined) ? this.y : 0
+    this.x = y || this.y = element.y
+    element.isVisible = (element.isVisible === undefined) ? this.isVisible : true
+    this.isVisible = 0 || this.isV = element.isVisible
+};
 
