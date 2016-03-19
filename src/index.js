10b14dacf9255f3eb8852cc500e8c9387010bd34
Olegnd <olegnd63@gmail.com>
2016-03-19 02:02:30 +0200

    modification 2 base.element.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ var BaseElement = require('./js/base.element.js');
 //var SnakePart = require('./js/snake.part.js');
 //var Snake = require('./js/snake.js');
 
-var elements = {
-    x : 10,
-    y : 10,
-    isVisible : false
-};
 
-console.log(new BaseElement(elements));
+//console.log(new BaseElement(elements));
 //console.log(new Food());
 //console.log(new SnakePart());
 //console.log(new Snake());
--- src/js/base.element.js
+++ src/js/base.element.js
@@ -4,11 +4,37 @@
 
 
 module.exports = function BaseElement (element) {
-    element.x = (element.x === undefined) ? this.x : 0
-    this.x = 0 || this.x = element.x
-    element.y = (element.x === undefined) ? this.y : 0
-    this.x = y || this.y = element.y
-    element.isVisible = (element.isVisible === undefined) ? this.isVisible : true
-    this.isVisible = 0 || this.isV = element.isVisible
+    if (element === undefined) { 
+        this.x = 0;
+        this.y = 0;  
+        this.isVisible = true;
+    }
+    else {
+        if (element === {}) { 
+        this.x = 0;
+        this.y = 0;  
+        this.isVisible = true;
+        }
+        else {
+            if (element.x === undefined) {
+                this.x = 0;
+            }
+            else {
+                this.x = element.x;
+            }
+            if (element.y === undefined) {
+                this.y = 0;
+            }
+            else {
+                this.y = element.y;
+            }
+            if (element.isVisible === undefined) {
+                this.isVisible = true;
+            }
+            else {
+                this.isVisible = element.isVisible;
+            }
+        }
+    }
 };
 
