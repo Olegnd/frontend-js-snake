19a736e2e4272dfcbc7acd15db9bfd0a0aaca15b
Olegnd <olegnd63@gmail.com>
2016-03-19 10:24:44 +0200

    modification 4 base.element.js

--- src/js/base.element.js
+++ src/js/base.element.js
@@ -4,37 +4,37 @@
 
 
 module.exports = function BaseElement (element) {
-    if (element === undefined) { 
+if (element === undefined) { 
+    this.x = 0;
+    this.y = 0;  
+    this.isVisible = true;
+}
+else {
+    if (element === {}) { 
         this.x = 0;
         this.y = 0;  
         this.isVisible = true;
     }
     else {
-        if (element === {}) { 
+        if (element.x === undefined) {
             this.x = 0;
-            this.y = 0;  
+        }
+        else {
+            this.x = element.x;
+        }
+        if (element.y === undefined) {
+            this.y = 0;
+        }
+        else {
+            this.y = element.y;
+        }
+        if (element.isVisible === undefined) {
             this.isVisible = true;
         }
         else {
-            if (element.x === undefined) {
-                this.x = 0;
-            }
-            else {
-                this.x = element.x;
-            }
-            if (element.y === undefined) {
-                this.y = 0;
-            }
-            else {
-                this.y = element.y;
-            }
-            if (element.isVisible === undefined) {
-                this.isVisible = true;
-            }
-            else {
-                this.isVisible = element.isVisible;
-            }
+            this.isVisible = element.isVisible;
         }
     }
+}
 };
 
