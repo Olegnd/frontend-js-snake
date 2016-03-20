e55d19d55e404c7c579feb93774a90fdd9151eb6
Olegnd <olegnd63@gmail.com>
2016-03-20 12:06:05 +0200

    final base.element.js

--- src/js/base.element.js
+++ src/js/base.element.js
@@ -1,29 +1,7 @@
 'use strict';
 
 //code Olegnd 
-/* Костя, воспользовался Тернарным оператором, но!,
-объясни пожалуйста, почему так не работает:
-
-element === undefined ? this.x = 0; this.y = 0;  this.isVisible = true;
-
-так тоже пробовал:
-
-element === undefined ? this.x = 0 && this.y = 0 && this.isVisible = true;
-
-дает ошибку ReferenceError: Invalid left-hand side in assignment(…) в обоих случаях
-я так понимаю не верно то что с лева от ? - 
-element === undefined ?,
-
-хотел сделать код в пять строк, как мы обсуждали с тобой в начале, а именно: 
-
-element === undefined ? this.x = 0; this.y = 0;  this.isVisible = true;
-element === {}        ? this.x = 0; this.y = 0;  this.isVisible = true;
-element.x === undefined ? this.x = 0 : this.x = element.x; 
-element.y === undefined ? this.y = 0 : this.y = element.y;
-element.isVisible === undefined ? this.isVisible = true : this.isVisible = element.isVisible;
-
- а смог сделать используя Тернарный оператор только так:
- */
+// version 1
 
 module.exports = function BaseElement (element) {
 if (element === undefined) {
@@ -47,6 +25,8 @@ else {
 
 
 /*
+version 2
+
 module.exports = function BaseElement (element) {
 if (element === undefined) { 
     this.x = 0;
