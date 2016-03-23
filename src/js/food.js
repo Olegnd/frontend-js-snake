f8aa31f1d341dfbbee9cc4589b274efd23789bb2
Olegnd <olegnd63@gmail.com>
2016-03-23 14:04:23 +0200

    modification 9 food.js

--- src/js/food.js
+++ src/js/food.js
@@ -4,17 +4,19 @@
 
 
 module.exports = function Food(elements){
-console.log('import object in Food ',elements);
+//console.log('import object in Food ',elements);
     this.x = elements.x;
     this.y = elements.y;
-    this.isVisible. = elements.isVisible;
-}
+    this.isVisible = elements.isVisible;
 Food.prototype = Object.create(elements.prototype);
+Food.prototype.constructor = Food;
 Food.prototype.feed = function(){
                         this.isVisible = false;
                       }
+}
 
-Food.prototype.constructor = Food;
+
+
 
 
 
