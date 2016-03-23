fe0255b4165552fbb01bebc2a55d5500a1261ddb
Olegnd <olegnd63@gmail.com>
2016-03-23 12:37:06 +0200

    modification 5 food.js index.js

--- src/js/food.js
+++ src/js/food.js
@@ -4,29 +4,31 @@
 //BaseElement = require('./js/base.element.js');
 
 module.exports = function Food(elements){
-console.log('import object in Food ',elements);
-
+//console.log('import object in Food ',elements);
+/*
 function BaseElement(elements) {    
     this.x = elements.x;
     this.y = elements.y;
     this.isVisible = elements.isVisible;
-}
+}*/
 
-console.log('create object by constructor BaseElement in Food.js  ',new BaseElement(elements));
+//console.log('create object by constructor BaseElement in Food.js  ',new BaseElement(elements));
     
-BaseElement.prototype.feed = function(){
+
+function food(elements){
+    console.log(food.call(this,elements));
+    
+}
+food.prototype.feed = function(){
     this.isVisible = false;
     }
-function Food(elements){
-    BaseElement.call(this,elements);
-}
     
 
 console.log('create object by constructor Food with method feed from in Food.js  ',
-            new Food(elements));
+            new food(elements));
     
 Food.prototype = Object.create(BaseElements.prototype);    
-Food.prototype.constructor = Food;
+food.prototype.constructor = Food;
 }
     
     
